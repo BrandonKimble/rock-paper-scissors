cf06ec2d86b0856e6d9190093c854e623db62c9e
Brandon Kimble <kimble.brandonm@gmail.com>
2021-05-22 16:35:46 -0500

    full game is finished, playable in the console

--- index.js
+++ index.js
@@ -1,29 +1,53 @@
 function computerPlay() {
-    const moves = ["rock", "paper", "scissors"]
+    const moves = ["rock", "paper", "scissors"];
     const random = Math.floor(Math.random() * moves.length);
-    console.log(random, moves[random]);
+    return moves[random];
 }
 
 function playRound(playerSelection, computerSelection) {
-    const player = playerSelection.toLowerCase;
-    const computer = computerSelection.toLowerCase;
+    var player = playerSelection.toLowerCase();
+    var computer = computerSelection.toLowerCase();
     if (player === "rock" && computer === "scissors") {
-        return "You Win! Rock beats Scissors";
+        return [1, "You Win! Rock beats Scissors"];
     } else if (player === "paper" && computer === "rock") {
-        return "You Win! Paper beats Rock";
+        return [1, "You Win! Paper beats Rock"];
     } else if (player === "scissors" && computer === "paper") {
-        return "You Win! Scissors beats Paper";
+        return [1, "You Win! Scissors beats Paper"];
     } else if (player === "rock" && computer === "paper") {
-        return "You Lose! Paper beats Rock";
+        return [-1, "You Lose! Paper beats Rock"];
     } else if (player === "paper" && computer === "scissors") {
-        return "You Lose! Scissors beats Paper";
+        return [-1, "You Lose! Scissors beats Paper"];
     } else if (player === "scissors" && computer === "rock") {
-        return "You Lose! Rock beats Scissors";
+        return [-1, "You Lose! Rock beats Scissors"];
     } else if (player === "rock" && computer === "rock") {
-        return "You Tied!";
+        return [0, "You Tied!"];
     } else if (player === "paper" && computer === "paper") {
-        return "You Tied!";
+        return [0, "You Tied!"];
     } else if (player === "scissors" && computer === "scissors") {
-        return "You Tied!";
+        return [0, "You Tied!"];
     } 
 }
+
+function game() {
+    var score = 0;
+    for (var i = 0; i < 5; i++) {
+        var computer = computerPlay();
+        var player = prompt("Rock, Paper, or Scissors?:");
+        var results = playRound(player, computer);
+        score += results[0];
+        console.log(results[1]);
+    } 
+    switch (true) {
+        case (score < 0):
+            console.log("Game Over. You Lost.");
+            break;
+        case (score > 0):
+            console.log("Game Over. You Won!");
+            break;
+        default:
+            console.log("Game Over. You Tied!");
+            break;
+    }
+}
+
+game();
